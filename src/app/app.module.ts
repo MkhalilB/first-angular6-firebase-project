6d64d8f1c3d7027bfb94ed2d9db5db7b6e746939
MkhalilB <medkhalil.bahri@gmail.com>
2018-11-11 22:17:01 +0100

    commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AcceuilComponent
   ],
   imports: [
     BrowserModule
